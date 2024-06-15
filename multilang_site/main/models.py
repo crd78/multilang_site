6fa7508e2aa6a36a45310beae0acca23a187788b
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-06-15 15:29:54 +0200

    finito

--- multilang_site/main/models.py
+++ multilang_site/main/models.py
@@ -1,9 +1,12 @@
+# models.py
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    publication_date = models.DateTimeField()
+    title = models.CharField(_('title'), max_length=200)
+    content = models.TextField(_('content'))
+    publication_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
